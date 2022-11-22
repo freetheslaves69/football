19237ef2364c8baa2b61d90b2c5f6b17a2d6991c
freetheslaves69 <116140783+freetheslaves69@users.noreply.github.com>
2022-11-22 12:04:22 +0700

    Add files via upload

---
+++ football_wile.py
@@ -0,0 +1,6 @@
+football_team = []
+repeat = "Y"
+while repeat == "Y" : 
+    name = input ("enter ther player's name: ")
+    football_team = input("do you want to enter another name(Y/N): ")
+print(football_team)
